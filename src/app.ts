ab3c0436673fb458c1e9022152fc505947b8ca5f
Tijani Fulani <Fulani07@gmail.com>
2021-09-17 10:23:20 +0100

    fix: updated my cors origin from 3030 to 3000

--- src/app.ts
+++ src/app.ts
@@ -24,11 +24,11 @@ const app = express();
 
 app.disable('x-powered-by');
 
-app.use(cors({ origin: 'http://localhost:3030', credentials: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(express.urlencoded({ extended: true }));
 
 /* SETUP ROUTES */
 app.use(authRouter);
