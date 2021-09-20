119f269e37511e7543f261c64831c88835a46c42
Tijani Fulani <Fulani07@gmail.com>
2021-09-20 18:18:01 +0100

    ..

--- src/Auth/routes.ts
+++ src/Auth/routes.ts
@@ -6,7 +6,7 @@ import { loginUser, logOutUser } from './controller';
 const router = Router();
 
 router.route('/login').post(loginUser);
-router.route('/logout').post(logOutUser);
+router.route('/logout').get(logOutUser);
 router.route('/signup').post(createAUser);
 
 export default router;
